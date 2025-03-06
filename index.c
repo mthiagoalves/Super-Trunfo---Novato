ff20d143715ccb9bcb0ae4d2bc0cfa940ad88509
mthiagoalves <m.thiago.ales@live.com>
2025-03-06 09:17:27 -0300

    feat(index.c): Data input for the first card

--- index.c
+++ index.c
@@ -20,4 +20,20 @@ int main()
     float area2;
     float gdp2;
     int touristPoints2;
+
+    // Data input for the first card
+    printf("Digite o estado da primeira carta (A-H): ");
+    scanf(" %c", &state1);
+    printf("Digite o código da primeira carta (Ex: A01): ");
+    scanf(" %s", code1);
+    printf("Digite o nome da cidade: ");
+    scanf(" %s", cityName1);
+    printf("Digite a população: ");
+    scanf(" %d", &population1);
+    printf("Digite a área da cidade (em km²): ");
+    scanf(" %f", &area1);
+    printf("Digite o PIB da cidade (em bilhões de reais): ");
+    scanf(" %f", &gdp1);
+    printf("Digite o número de pontos turísticos: ");
+    scanf(" %d", &touristPoints1);
 }
