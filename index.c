6b3c7f5584f77646ee354aa309930ae08f6f7fb2
mthiagoalves <m.thiago.ales@live.com>
2025-03-06 09:23:32 -0300

    feat(index.c): Display of information for the first card

--- index.c
+++ index.c
@@ -36,4 +36,30 @@ int main()
     scanf(" %f", &gdp1);
     printf("Digite o número de pontos turísticos: ");
     scanf(" %d", &touristPoints1);
+
+    // Data input for the second card
+    printf("\nDigite o estado da segunda carta (A-H): ");
+    scanf(" %c", &state2);
+    printf("Digite o código da segunda carta (Ex: B02): ");
+    scanf(" %s", code2);
+    printf("Digite o nome da cidade: ");
+    scanf(" %s", cityName2);
+    printf("Digite a população: ");
+    scanf(" %d", &population2);
+    printf("Digite a área da cidade (em km²): ");
+    scanf(" %f", &area2);
+    printf("Digite o PIB da cidade (em bilhões de reais): ");
+    scanf(" %f", &gdp2);
+    printf("Digite o número de pontos turísticos: ");
+    scanf(" %d", &touristPoints2);
+
+    // Display of information for the first card
+    printf("\nCarta 1:\n");
+    printf("Estado: %c\n", state1);
+    printf("Código: %s\n", code1);
+    printf("Nome da Cidade: %s\n", cityName1);
+    printf("População: %d\n", population1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", gdp1);
+    printf("Número de Pontos Turísticos: %d\n", touristPoints1);
 }
