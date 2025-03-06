56cc06c185b1b1796afa21e126723cb8bb03f8f1
mthiagoalves <m.thiago.ales@live.com>
2025-03-06 09:15:38 -0300

    feat(index.c): Declaration of variables for the second card

--- index.c
+++ index.c
@@ -2,11 +2,22 @@
 
 int main()
 {
-    char state;
-    char letterCode[5];
-    char city[20];
-    int population;
-    float area;
-    float pib;
-    int turisticPlaces;
+
+    // Declaration of variables for the first card
+    char state1;
+    char code1[4];
+    char cityName1[50];
+    int population1;
+    float area1;
+    float gdp1;
+    int touristPoints1;
+
+    // Declaration of variables for the second card
+    char state2;
+    char code2[4];
+    char cityName2[50];
+    int population2;
+    float area2;
+    float gdp2;
+    int touristPoints2;
 }
